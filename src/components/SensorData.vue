7375e80164b4c1ef9fad844fb050c696014c21f3
whitepodo <ed0909@naver.com>
2025-05-22 23:47:12 +0900

    첫번째 수정

--- src/components/SensorData.vue
+++ src/components/SensorData.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h2>📊 센서 데이터</h2>
+
+    <select v-model="selectedCollection" @change="loadData">
+      <option value="First_Car">First_Car</option>
+      <option value="Second_Car">Second_Car</option>
+    </select>
+
     <ul>
       <li v-for="item in sensorData" :key="item.id">
-        🕒 {{ item.timestamp?.toDate().toLocaleString() }} |
-        🌡 온도: {{ item.temp }} |
+        🕒 {{ item.time?.toDate().toLocaleString() }} |
+        🌡️ 온도: {{ item.temp }} |
         💧 습도: {{ item.humi }} |
-        🧪 가스: {{ item.gas }}
+        🪨 가스: {{ item.gas }}
       </li>
     </ul>
   </div>
@@ -17,16 +23,22 @@ import { ref, onMounted } from 'vue'
 import { db } from '../firebase'
 import { collection, getDocs } from 'firebase/firestore'
 
+const selectedCollection = ref('First_Car')
 const sensorData = ref([])
 
-onMounted(async () => {
-  const querySnapshot = await getDocs(collection(db, 'esp32'))
+const loadData = async () => {
+  sensorData.value = []
+  const querySnapshot = await getDocs(collection(db, selectedCollection.value))
   querySnapshot.forEach((doc) => {
     sensorData.value.push({
       id: doc.id,
       ...doc.data()
     })
   })
+}
+
+onMounted(() => {
+  loadData()
 })
 </script>
 
@@ -37,5 +49,8 @@ h2 {
 }
 li {
   margin: 0.5rem 0;
+}
+select {
+  margin-bottom: 1rem;
 }
 </style>
